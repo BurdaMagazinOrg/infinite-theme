fc2d6b638feea874556c4d9c1bb4a50269dce9f5
Michal Landsman <landsman@studioart.cz>
2017-02-14 20:54:33 +0100

    Fixed BrowserSync, Removed compass mixins

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,18 +1,15 @@
-/**
- * Project based environment variables
- * @type {string}
- */
-var domain_bs	= 'http://thunder.dev:8888'; // @todo: think about better solution
-var port_bs		= 8888; // @todo: think about better solution
+var gulp            = require('gulp'),
+    bower		    = require('gulp-bower'),
+    sequence	    = require('gulp-sequence'),
+    concat		    = require('gulp-concat'),
+    sass		    = require('gulp-sass'),
+    sourcemaps	    = require('gulp-sourcemaps'),
+    prefixer	    = require('gulp-autoprefixer'),
+    readlineSync    = require('readline-sync'),
+    browserSync     = require('browser-sync').create(),
+    environments    = ['dev', 'stage', 'prod'],
+    environment     = environments[3];
 
-var gulp		= require('gulp'),
-    bower		= require('gulp-bower'),
-    sequence	= require('gulp-sequence'),
-    concat		= require('gulp-concat'),
-    sass		= require('gulp-sass'),
-    sourcemaps	= require('gulp-sourcemaps'),
-    prefixer	= require('gulp-autoprefixer'),
-    browserSync = require('browser-sync').create();
 
 gulp.task('bower', function()
 {
@@ -25,31 +22,48 @@ gulp.task('bower', function()
 gulp.task('copy-fonts', function ()
 {
     return gulp.src(["./bower_components/ionicons/fonts/*"])
-        .pipe(gulp.dest('./fonts/ionicons'));
+               .pipe(gulp.dest('./fonts/ionicons'));
 });
+
 
 gulp.task('sass', function ()
 {
-    return gulp.src('./sass/infinite-global.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }))
-        .pipe(prefixer({
-            browsers: ['last 3 versions', 'IE 9'],
-            cascade: false
-        }))
-        .pipe(sourcemaps.write())
-        .pipe(concat('infinite-global.css'))
-        .pipe(gulp.dest('./css'))
-        .pipe(browserSync.stream());
+    if(process.argv[3] === '--' + environments[0])
+    {
+        return gulp
+            .src('./sass/infinite-global.scss')
+            .pipe(sourcemaps.init())
+            .pipe(sass())
+            .pipe(prefixer({
+                browsers: ['last 3 versions', 'IE 9'],
+                cascade: false
+            }))
+            .pipe(sourcemaps.write())
+            .pipe(concat('infinite-global.css'))
+            .pipe(gulp.dest('./css'))
+            .pipe(browserSync.stream());
+    }
+    else
+    {
+        return gulp
+            .src('./sass/infinite-global.scss')
+            .pipe(sass({
+                outputStyle: 'compressed'
+            }))
+            .pipe(prefixer({
+                browsers: ['last 3 versions', 'IE 9'],
+                cascade: false
+            }))
+            .pipe(concat('infinite-global.css'))
+            .pipe(gulp.dest('./css'));
+    }
 });
+
 
 gulp.task('watch', function ()
 {
     browserSync.init({
-        proxy: domain_bs,
-        port: port_bs
+        proxy: readlineSync.question('Virtual host (e.g. http://thunder.dev:8888): ')
     });
 
     gulp.watch('./sass/**.s+(a|c)ss', ['sass'])
@@ -62,6 +76,7 @@ gulp.task('watch', function ()
     gulp.watch('./templates/**.html.twig')
         .on('change', browserSync.reload);
 });
+
 
 gulp.task('default', function(callback)
 {
---
+++ README.md
@@ -0,0 +1,15 @@
+# Infinite
+
+awesome Drupal 8 theme delivered with Thunder CMS.
+
+### Installation
+
+1. go to theme path in bash: `cd docroot/themes/contrib/infinite_theme` 
+2. type: `npm install` for build theme for production use
+
+### Development
+
+`gulp` build theme for production use (starting after npm install)
+`gulp watch` auto-builds if detected change in sass / templates / js and reload browser thanks BrowserSync
+`gulp sass --dev` generate readable CSS with source maps
+`gulp bower` just run bower.json update
