0db305e0062df590f634183ebacf807749ed72a9
Markus Goedecke <markus.goedecke@one-pixel-ahead.com>
2017-11-27 08:45:14 +0100

    add addToCart and removeFromCart options to tracking manager

--- js/infinite/managers/tracking-manager.js
+++ js/infinite/managers/tracking-manager.js
@@ -395,6 +395,20 @@
             }
           }
           break;
+        case 'addToCart':
+          tmpTrackingObject.event = 'addToCart';
+          tmpTrackingObject.ecommerce = {
+              currencyCode: item.currencyCode,
+              products: [pData]
+          };
+          break;
+        case 'removeFromCart':
+          tmpTrackingObject.event = 'removeFromCart';
+          tmpTrackingObject.ecommerce = {
+              currencyCode: item.currencyCode,
+              products: [pData]
+          };
+          break;
         default:
           return;
       }
