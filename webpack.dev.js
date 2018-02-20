4022cb9c4df956c88553528f9574ef572b2503ef
Timo Welde <tjwelde@gmail.com>
2018-02-20 17:10:08 +0100

    INREL-3418: add circular dependency plugin

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,8 +1,18 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
+const CircularDependencyPlugin = require('circular-dependency-plugin')
+
 module.exports = merge(common, {
   devtool: 'eval',
   plugins: [
+    new CircularDependencyPlugin({
+      // exclude detection of files based on a RegExp
+      exclude: /a\.js|node_modules/,
+      // add errors to webpack instead of warnings
+      // failOnError: true,
+      // set the current working directory for displaying module paths
+      cwd: `${process.cwd()}/js/infinite`,
+    })
   ]
 });
