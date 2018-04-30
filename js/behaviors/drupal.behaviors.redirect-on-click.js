dcc60dab0d21fb74f508e46eefb11eb140f26e6d
Savio van Hoi <savio.nguyenvanhoi@burda.com>
2018-04-30 15:26:20 +0200

    INREL-3901: Fix external URL

--- js/behaviors/drupal.behaviors.redirect-on-click.js
+++ js/behaviors/drupal.behaviors.redirect-on-click.js
@@ -1,25 +1,25 @@
 (function ($, Drupal, drupalSettings, Backbone, window) {
   Drupal.behaviors.redirectOnClick = {
     attach: function () {
-      this.onClickRedirectTo('[data-internal-url]', false);
-      this.onClickRedirectTo('[data-external-url]', true);
+      this.onClickRedirectTo('data-internal-url', false);
+      this.onClickRedirectTo('data-external-url', true);
     },
     onClickRedirectTo: function (urlDataAttribute, openInNewWindow) {
 
       // Drupal context is not used
       // to make sure that events are always captured
-      $(document).on('click', urlDataAttribute, function (e) {
+      $(document).on('click', '[' + urlDataAttribute + ']', function (e) {
         let currentTarget = e.currentTarget;
-        let internalURL = currentTarget.getAttribute('data-internal-url');
+        let redirectUrl = currentTarget.getAttribute(urlDataAttribute);
         let target = openInNewWindow
           ? currentTarget.getAttribute('data-target') || '_blank'
           : currentTarget.getAttribute('data-target');
         let hasTarget = !!target;
 
         if (hasTarget) {
-          window.open(internalURL, target);
+          window.open(redirectUrl, target);
         } else {
-          location.href = internalURL;
+          location.href = redirectUrl;
         }
       });
     }
