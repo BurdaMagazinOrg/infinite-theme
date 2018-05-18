cdc05833d25284e2fad0f79188a420b54814374c
Savio van Hoi <savio.nguyenvanhoi@burda.com>
2018-05-18 14:15:07 +0200

    INREL-4032: Fix bug with event bound multiple times

--- js/behaviors/drupal.behaviors.redirect-on-click.js
+++ js/behaviors/drupal.behaviors.redirect-on-click.js
@@ -1,27 +1,28 @@
-(function ($, Drupal, drupalSettings, Backbone, window) {
+(function($, Drupal, drupalSettings, Backbone, window) {
   Drupal.behaviors.redirectOnClick = {
-    attach: function () {
+    attach: function() {
       this.onClickRedirectTo('data-internal-url', false);
       this.onClickRedirectTo('data-external-url', true);
     },
-    onClickRedirectTo: function (urlDataAttribute, openInNewWindow) {
-
+    onClickRedirectTo: function(urlDataAttribute, openInNewWindow) {
       // Drupal context is not used
       // to make sure that events are always captured
-      $(document).on('click', '[' + urlDataAttribute + ']', function (e) {
-        let currentTarget = e.currentTarget;
-        let redirectUrl = currentTarget.getAttribute(urlDataAttribute);
-        let target = openInNewWindow
-          ? currentTarget.getAttribute('data-target') || '_blank'
-          : currentTarget.getAttribute('data-target');
-        let hasTarget = !!target;
+      $(document)
+        .off('click.redirect')
+        .on('click.redirect', '[' + urlDataAttribute + ']', function(e) {
+          let currentTarget = e.currentTarget;
+          let redirectUrl = currentTarget.getAttribute(urlDataAttribute);
+          let target = openInNewWindow
+            ? currentTarget.getAttribute('data-target') || '_blank'
+            : currentTarget.getAttribute('data-target');
+          let hasTarget = !!target;
 
-        if (hasTarget) {
-          window.open(redirectUrl, target);
-        } else {
-          location.href = redirectUrl;
-        }
-      });
+          if (hasTarget) {
+            window.open(redirectUrl, target);
+          } else {
+            location.href = redirectUrl;
+          }
+        });
     }
   };
 })(jQuery, Drupal, drupalSettings, Backbone, window);
