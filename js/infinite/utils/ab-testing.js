3d9957bde4cbdd0d2f0d239b8e3e73614b6bcbb8
Dan Schalow <daniel.schalow@burda.com>
2018-08-01 16:49:42 +0200

    INREL-4238: added method for checking whether feature should be displayed

--- js/infinite/utils/ab-testing.js
+++ js/infinite/utils/ab-testing.js
@@ -1,17 +1,14 @@
-/**
- * @file
- * Sets group A/B for A/B Tests.
- */
-
 function ABTest(feature) {
   this.init = () => {
-    this.assignedGroups = this.getAbGroupFromLocalStorage() || [];
+    this.assignedGroups = JSON.parse(window.localStorage.getItem('abTests')) || [];
     this.abTest = {
       feature,
       featureActive: true,
       abTestActive: true,
     };
     this.abTest.group = this.getABGroup() || this.setABGroup();
+
+    Object.assign(this.abTest, this.getTestFromLocalStorage());
 
     if (!this.getABGroup()) {
       this.assignedGroups.push(this.abTest);
@@ -20,13 +17,20 @@ function ABTest(feature) {
     window.dataLayer.push({ event: 'executeABTest', abTest: this.abTest });
   };
 
-  this.getAbGroupFromLocalStorage = () => JSON.parse(window.localStorage.getItem('abTests'));
+  this.getTestFromLocalStorage = () => this.assignedGroups.find(item => item.feature === this.abTest.feature);
 
   this.setABGroup = () => (Math.random() <= 0.5 ? 'a' : 'b');
 
   this.getABGroup = () => {
     const result = this.assignedGroups.find(obj => obj.feature === this.abTest.feature, this);
     return !!result && result.group;
+  };
+
+  this.showUserContent = () => {
+    const abSettingsAllow = !this.abTest.abTestActive
+    || (this.abTest.abTestActive && this.getTestFromLocalStorage().group === 'a');
+    const active = this.abTest.featureActive && abSettingsAllow;
+    return abSettingsAllow && active;
   };
 
   this.init();
