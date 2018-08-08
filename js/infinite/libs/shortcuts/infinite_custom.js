133e676c49d892f330ce6cf5413b557a7ecbfea7
Nico Davis <nicod@gmx.net>
2018-08-08 10:57:53 +0200

    INREL4308 change lazyloading event to eventNonInteraction: true

--- js/infinite/libs/shortcuts/infinite_custom.js
+++ js/infinite/libs/shortcuts/infinite_custom.js
@@ -104,7 +104,7 @@
       TrackingManager.trackEvent({
         category: 'lazy-loading',
         action: this.$more.attr('href'),
-        eventNonInteraction: false
+        eventNonInteraction: true
       });
     }
 
