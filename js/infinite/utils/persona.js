c62aa8bbc5268b0d3f103f23b1acc4159d805089
Nico Davis <nicod@gmx.net>
2018-11-05 10:52:53 +0100

    INREL-0 fix empty reduce behavior

--- js/infinite/utils/persona.js
+++ js/infinite/utils/persona.js
@@ -1,26 +1,33 @@
 window.Persona = (function Persona() {
   const that = {};
-  const channelTypes = ['channel', 'subChannel'];
+  const channelTypes = ["channel", "subChannel"];
   let personaCollection;
 
   const getPersonaInfos = () => {
-    const channel = window.document.head.querySelector("[property='article:section']");
-    const subChannel = window.document.head.querySelector("[itemprop='acquia_lift:content_section']");
+    const channel = window.document.head.querySelector(
+      "[property='article:section']"
+    );
+    const subChannel = window.document.head.querySelector(
+      "[itemprop='acquia_lift:content_section']"
+    );
     const persona = {
       channel: !!channel && channel.content,
-      subChannel: !!subChannel && subChannel.content,
+      subChannel: !!subChannel && subChannel.content
     };
 
     return !!persona.channel && !!persona.subChannel ? persona : false;
   };
 
-  const getHighestValueByKey = key => Object.keys(personaCollection[key]).reduce(
-    (a, b) => (personaCollection[key][a] > personaCollection[key][b] ? a : b),
-  );
+  const getHighestValueByKey = key => {
+    Object.keys(personaCollection[key]).reduce(
+      (a, b) => (personaCollection[key][a] > personaCollection[key][b] ? a : b),
+      0
+    );
+  };
 
   const getPersona = () => {
     const persona = {};
-    channelTypes.forEach((type) => {
+    channelTypes.forEach(type => {
       persona[type] = getHighestValueByKey(type);
     });
 
@@ -28,28 +35,33 @@ window.Persona = (function Persona() {
   };
 
   const incrementChannel = (persona, channel) => {
-    Object.keys(persona).forEach((key) => {
+    Object.keys(persona).forEach(key => {
       if (key === channel) {
         persona[key] += 1;
       }
     });
   };
 
-  const writePersonaCollection = (persona) => {
-    channelTypes.forEach((type) => {
+  const writePersonaCollection = persona => {
+    channelTypes.forEach(type => {
       const channel = persona[type]; // channelName - Beauty as example
       const channelCollection = personaCollection[type]; // channel or subChannel;
-      if (!channelCollection.hasOwnProperty(channel)) channelCollection[channel] = 0;
+      if (!channelCollection.hasOwnProperty(channel))
+        channelCollection[channel] = 0;
       incrementChannel(channelCollection, channel);
     });
 
-    window.localStorage.setItem('personaCollection', JSON.stringify(personaCollection));
+    window.localStorage.setItem(
+      "personaCollection",
+      JSON.stringify(personaCollection)
+    );
   };
 
-  const getPersonaCollection = () => JSON.parse(window.localStorage.getItem('personaCollection'));
+  const getPersonaCollection = () =>
+    JSON.parse(window.localStorage.getItem("personaCollection"));
   const getDefaultCollection = () => {
     const obj = {};
-    channelTypes.forEach((data) => {
+    channelTypes.forEach(data => {
       obj[data] = {};
     });
 
@@ -62,7 +74,7 @@ window.Persona = (function Persona() {
 
     if (personaInfos) {
       writePersonaCollection(personaInfos);
-      window.dataLayer.push({ event: 'executePersona', persona: getPersona() });
+      window.dataLayer.push({ event: "executePersona", persona: getPersona() });
     }
   };
 
@@ -70,4 +82,4 @@ window.Persona = (function Persona() {
   that.getPersonaCollection = getPersonaCollection;
   that.getPersona = getPersona;
   return that;
-}());
+})();
