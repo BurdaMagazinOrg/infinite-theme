7258bf56a02520d7fb4742c3c7635891e12e6d83
Nico Davis <nicodavis@Nicos-MBP.fritz.box>
2018-11-28 15:16:08 +0100

    INREL-4974 implement newsletter block

--- js/behaviors/drupal.behaviors.newsletter.js
+++ js/behaviors/drupal.behaviors.newsletter.js
@@ -39,6 +39,7 @@
         if (this.useOptin) this.initOptinComponents();
       },
       initOptinComponents() {
+        this.permissions = window.newsletterPermissions;
         this.el.classList.add("optin");
         this.optinCheckbox = this.el.querySelector(".newsletter__checkbox");
         this.optinText = this.el.querySelector(".newsletter__optin-text");
