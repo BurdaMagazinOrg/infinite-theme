015ba66594888dc4339a78ba66dc483eef720100
Nico Davis <nicodavis@Nicos-MBP.fritz.box>
2019-01-15 14:39:48 +0100

    INREL-5246 sidebar array test

--- js/behaviors/drupal.behaviors.sidebar-navigation.js
+++ js/behaviors/drupal.behaviors.sidebar-navigation.js
@@ -1,12 +1,18 @@
 (function(Drupal, drupalSettings, Backbone) {
   const SidebarNavigationBackboneView = Backbone.View.extend({
     context: null,
+    lastScroll: 0,
+    isScrollDirectionDown: false,
+    lastIntersectionRatio: 0,
+    intersectionArray: [],
     observer: null,
+    observerInactive: false,
     scrollTargets: null,
     observerOptions: {
       root: null,
-      rootMargin: "56px",
+      rootMargin: "-56px",
       threshold: [0]
+      // threshold: [0, 0.25, 0.5, 0.75, 1]
     },
     initialize(options) {
       this.context = options.context || document;
@@ -16,49 +22,101 @@
       );
       this.initScrollToBehavior();
       this.collectScrollTargets();
+      this.addListener();
     },
     initScrollToBehavior() {
       Array.from(this.navigationLinks).forEach(element => {
         const liEl = element.parentElement;
-        liEl.addEventListener("click", e => {
-          e.preventDefault();
-          const name = element.getAttribute("id");
-          const paragraph = this.context.querySelector(
-            `.item-paragraph[name="${name}"]`
-          );
-          const rect = paragraph.getBoundingClientRect();
 
-          window.scrollTo({
-            top: Math.max(0, rect.top + window.scrollY - 66),
-            behavior: "smooth"
-          });
-        });
+        liEl.addEventListener("click", e =>
+          this.handleNavigationClick(e, element)
+        );
       });
+    },
+    addListener() {
+      window.addEventListener("scroll", this.handleScroll.bind(this));
+    },
+    handleScroll(e) {
+      this.isScrollDirectionDown = this.lastScroll < window.scrollY;
+      this.lastScroll = window.scrollY;
+    },
+    handleNavigationClick(e, element) {
+      e.preventDefault();
+      const name = element.getAttribute("id");
+      const paragraph = this.context.querySelector(
+        `.item-paragraph[name="${name}"]`
+      );
+      const rect = paragraph.getBoundingClientRect();
+      const diff = window.screen.width >= 756 ? 66 : 300;
+      const yPos = rect.top + window.scrollY - diff;
+
+      if (this.el.classList.contains("btn__open-tree--is-open")) {
+        this.el.classList.remove("btn__open-tree--is-open");
+      }
+
+      // window.scrollTo({top: yPos, behavior: "smooth"});
+      // crossbrowser behavior
+      this.highlight(element);
+      this.observerInactive = true;
+      jQuery("html, body").animate(
+        { scrollTop: Math.max(0, yPos) },
+        800,
+        () => {
+          this.observerInactive = false;
+        }
+      );
     },
     collectScrollTargets() {
-      let observer = null;
+      const observer = new IntersectionObserver(
+        this.intersectionHandler.bind(this),
+        this.observerOptions
+      );
 
-      Array.from(this.scrollTargets).forEach(element => {
-        observer = new IntersectionObserver(
-          this.intersectionHandler.bind(this),
-          this.observerOptions
-        );
-        observer.observe(element);
-      });
+      Array.from(this.scrollTargets).forEach(el => observer.observe(el));
     },
     intersectionHandler(entries, observer) {
-      let name = "";
-      let selectedElement = null;
-
       entries.forEach(entry => {
+        console.log(
+          "intersectionHandler",
+          entry.target.getAttribute("name"),
+          entry,
+          this.observerOptions.threshold,
+          this.intersectionArray.map(e => e.getAttribute("name"))
+        );
+
+        if (!entry.isIntersecting) {
+          if (this.intersectionArray.length > 0) {
+            if (this.isScrollDirectionDown) {
+              this.intersectionArray.shift();
+              this.highlightByScrollTarget(this.intersectionArray[0]);
+            } else {
+              this.intersectionArray.pop();
+            }
+          }
+        }
+
         if (entry.isIntersecting) {
-          this.removeHighlighting();
-          name = entry.target.getAttribute("name");
-          selectedElement = this.el.querySelector(`[id="${name}"]`);
-          if (selectedElement)
-            selectedElement.parentElement.classList.add("is-active");
+          this.intersectionArray.push(entry.target);
+          const element = this.isScrollDirectionDown
+            ? this.intersectionArray[0]
+            : entry.target;
+
+          this.highlightByScrollTarget(element);
         }
       });
+    },
+    highlightByScrollTarget(el) {
+      if (this.observerInactive) return;
+
+      const name = el.getAttribute("name");
+      const selectedElement = this.el.querySelector(`[id="${name}"]`);
+      if (selectedElement) {
+        this.highlight(selectedElement);
+      }
+    },
+    highlight(el) {
+      this.removeHighlighting();
+      el.parentElement.classList.add("is-active");
     },
     removeHighlighting() {
       const elements = this.el.querySelectorAll(".is-active") || [];
