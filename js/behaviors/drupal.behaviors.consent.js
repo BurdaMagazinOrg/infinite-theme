b8ee5aeeebac3c36cfd2b43bc332e8060430e987
Nico Davis <n.davis@nicodavis.de>
2020-06-08 14:36:15 +0200

    INREL-7567 feat: remove gtm script and add it via drupal.behaviors.consent.js

--- js/behaviors/drupal.behaviors.consent.js
+++ js/behaviors/drupal.behaviors.consent.js
@@ -46,6 +46,17 @@
               a.appendChild(r);
             })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
           },
+        },
+        {
+          init: function () {},
+          data: [{ vendorId: 10211 }],
+          script: {
+            innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', '${window.gtm_id}');`,
+          },
         }
       );
       return { vendors: window.consentVendors };
